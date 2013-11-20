5e4e23d2d5e381d529071d4cd6d2b6ee22abe5a4
Reid Workman <workman@internetfaction.com>
2013-11-19 19:47:28 -0500

    First fix of the day.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  config.secret_key = AwesomeBlog::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  config.secret_key = if Rails.env.development? or Rails.env.test?
     ('x' * 128)
   else
     ENV['DEVISE_SECRET_TOKEN']
